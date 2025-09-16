892eeb5e0b46f7d61107b141c5f0e8b9f615ed6d
tuyenvui3 <tuyenvm@icebear.com.vn>
2025-09-16 08:30:54 +0700

    update v-1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.panda.rate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
