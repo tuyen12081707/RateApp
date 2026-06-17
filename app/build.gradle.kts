7e324650f1ab3d4207975a928f53f26e8d7cea1e
manhtuyen <protuyen2001@gmail.com>
2026-06-17 15:31:12 +0700

    update 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.panda.rate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 111
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
